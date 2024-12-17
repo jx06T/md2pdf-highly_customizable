ba8652f050cf6af6fc0062b76bd2adb99609c7b8
John Doe <johndoe@example.com>
2024-12-17 16:39:51 +0800

    t0

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
+
+declare module './reportWebVitals' {
+    const reportWebVitals: (onPerfEntry?: (entry: any) => void) => void;
+    export default reportWebVitals;
+  }
+  
