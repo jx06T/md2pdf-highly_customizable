28fe2565913226687cae7e44741a39516c20ec84
sfes13 <71823064+sfes13@users.noreply.github.com>
2024-12-21 13:05:54 +0800

    編輯器免強

--- src/components/EditorArea.tsx
+++ src/components/EditorArea.tsx
@@ -1,43 +1,42 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 
-import { directivesPlugin, GenericDirectiveEditor, MDXEditor, headingsPlugin, listsPlugin, quotePlugin, thematicBreakPlugin } from '@mdxeditor/editor'
-import '@mdxeditor/editor/style.css';
-import { type MDXEditorMethods, type MDXEditorProps, type DirectiveDescriptor } from '@mdxeditor/editor'
+import MDEditor, { commands } from '@uiw/react-md-editor';
 
 
-const CalloutDirectiveDescriptor: DirectiveDescriptor = {
-    name: 'callout',
-    testNode(node) {
-        return node.name === 'callout'
-    },
-    // set some attribute names to have the editor display a property editor popup.
-    attributes: [],
-    // used by the generic editor to determine whether or not to render a nested editor.
-    hasChildren: true,
-    Editor: GenericDirectiveEditor
-}
 
 function EditorArea({ width, expandLevel }: { width: number, expandLevel: number }) {
-    const ref = React.useRef<MDXEditorMethods>(null)
-
-    const [markdown, setMarkdown] = React.useState('# Initial Content');
-
-    const preSetMarkdown = (md: string) => {
-       const newMd = md.replaceAll('\\#', '#')
-        setMarkdown(newMd)
-        ref.current!.setMarkdown(newMd);
-        console.log(md,"\n---------------------\n",newMd)
-    }
+    const [mdValue, setMdValue] = useState("")
+    const handleChange = (val?: string) => {
+        if (val !== undefined) {
+            setMdValue(val);
+        }
+    };
     return (
         <div style={{ width: width }} className={`editor-area bg-black ${expandLevel > 1 ? "max-w-full-24" : (expandLevel > 0 ? "max-w-full-12" : "")} min-w-0 lg:min-w-48 flex-grow-0 flex-shrink-0`}>
             <div className=" p-1 h-full w-full">
                 {/* <textarea name="editor" id="editor" className=" w-full bg-black text-white  h-full">
                     {`tttttt\nttt\nadcewv3\nq2bg5`}
                 </textarea> */}
-
-                <MDXEditor ref={ref} onChange={preSetMarkdown} markdown={markdown} plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), thematicBreakPlugin()]} />
-
-
+                <MDEditor
+                    value={mdValue}
+                    onChange={handleChange}
+                    preview="edit"
+                    visibleDragbar={false}
+                    height="100%"
+                    // commands={[
+                    //     commands.bold,  // 粗體
+                    //     commands.italic,  // 斜體
+                    //     commands.strikethrough,  // 刪除線
+                    //     commands.hr,  // 分隔線
+                    //     commands.title,  // 標題
+                    //     commands.divider,  // 分隔符號
+                    //     commands.link,  // 連結
+                    //     commands.quote,  // 引用
+                    //     commands.code  // 程式碼
+                    // ]}
+                    extraCommands={[
+                    ]}
+                />
             </div>
         </div>
     )
