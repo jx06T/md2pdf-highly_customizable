992a61d3058af89b4e584dd6b782910c710ba23f
sfes13 <71823064+sfes13@users.noreply.github.com>
2024-12-28 13:18:37 +0800

    改渲染器

--- src/components/PreviewArea.tsx
+++ src/components/PreviewArea.tsx
@@ -1,47 +1,8 @@
 import React, { useEffect } from "react";
-import ReactMarkdown from "markdown-to-jsx";
+// import ReactMarkdown from "markdown-to-jsx";
 import { useMdContext } from "../context/MdContext";
-
-// function autoPageBreak() {
-//     const container = document.querySelector('.markdown-content');
-//     // @ts-ignore
-//     const contentHeight = container.offsetHeight;
-//     const pageHeight = 297 * 3.78; // A4 height in pixels (約等於 297mm)
-//     const margin = 20 * 3.78; // 20mm margins
-//     const effectiveHeight = pageHeight - (2 * margin);
-
-//     // 計算需要的頁數
-//     const pageCount = Math.ceil(contentHeight / effectiveHeight);
-
-//     // 插入分頁符
-//     for (let i = 1; i < pageCount; i++) {
-//         const breakPoint = i * effectiveHeight;
-//         insertPageBreak(container, breakPoint);
-//     }
-// }
-
-// // @ts-ignore
-// function insertPageBreak(container, position) {
-
-//     if (container.children.length < 2) {
-//         return
-//     }
-//     const elements = Array.from(container.firstchild.children);
-
-//     let currentHeight = 0;
-
-//     for (const element of elements) {
-//         // @ts-ignore
-//         currentHeight += element.offsetHeight;
-//         if (currentHeight > position) {
-//             const pageBreak = document.createElement('div');
-//             pageBreak.className = 'page-break';
-//             // @ts-ignore
-//             element.parentNode.insertBefore(pageBreak, element);
-//             break;
-//         }
-//     }
-// }
+import rehypeKatexNotranslate from 'rehype-katex-notranslate';
+import Markdown from 'react-markdown'
 
 function PreviewArea({ width, displayId, expandLevel }: { width: number, displayId: number, expandLevel: number }) {
     const { mdValue, setMdValue } = useMdContext()
@@ -76,33 +37,95 @@ function PreviewArea({ width, displayId, expandLevel }: { width: number, display
 
     };
 
-    return (
-        <div className={`preview-area no-scrollbar bg-stone-100 min-w-0 lg:min-w-48 w-full h-full overflow-y-scroll   ${expandLevel > 0 ? "flex-grow flex-shrink " : " absolute left-0 " + (displayId === 2 ? "" : " opacity-0 pointer-events-none ")} `} >
 
+    function autoPageBreak() {
+        const container = document.querySelector('.markdown-content');
+        // @ts-ignore
+        const contentHeight = container.offsetHeight;
+        const pageHeight = 297 * 3.78; // A4 height in pixels (約等於 297mm)
+        const margin = 20 * 3.78; // 20mm margins
+        const effectiveHeight = pageHeight - (2 * margin);
+
+        // 計算需要的頁數
+        const pageCount = Math.ceil(contentHeight / effectiveHeight);
+
+        // 插入分頁符
+        for (let i = 1; i < pageCount; i++) {
+            const breakPoint = i * effectiveHeight;
+            insertPageBreak(container, breakPoint);
+        }
+    }
+
+    // @ts-ignore
+    function insertPageBreak(container, position) {
+
+        if (container.children.length < 2) {
+            return
+        }
+        const elements = Array.from(container.firstchild.children);
+
+        let currentHeight = 0;
+
+        for (const element of elements) {
+            // @ts-ignore
+            currentHeight += element.offsetHeight;
+            if (currentHeight > position) {
+                const pageBreak = document.createElement('div');
+                pageBreak.className = 'page-break';
+                // @ts-ignore
+                element.parentNode.insertBefore(pageBreak, element);
+                break;
+            }
+        }
+    }
+    const components = {{
+        code(props) {
+            const { children, className, node, ...rest } = props
+            const match = /language-(\w+)/.exec(className || '')
+            return match ? (
+                <SyntaxHighlighter
+                    {...rest}
+                    PreTag="div"
+                    children={String(children).replace(/\n$/, '')}
+                    language={match[1]}
+                    style={dark}
+                />
+            ) : (
+                <code {...rest} className={className}>
+                    {children}
+                </code>
+            )
+        }
+    }
+}
+
+return (
+    <div className={`preview-area no-scrollbar bg-stone-100 min-w-0 lg:min-w-48 w-full h-full overflow-y-scroll   ${expandLevel > 0 ? "flex-grow flex-shrink " : " absolute left-0 " + (displayId === 2 ? "" : " opacity-0 pointer-events-none ")} `} >
+
+        <div
+            style={{
+                scale: width / 850
+            }}
+            className=" cover"
+        >
+        </div>
+        <div className=" preview-area-2 p-2 relative ">
             <div
                 style={{
                     scale: width / 850
                 }}
-                className=" cover"
+                className="border-2 page markdown-content"
             >
+                <Markdown options={options} rehypePlugins={[rehypeKatex]}>
+                    {mdValue.replace(
+                        /^([-]{3,})$/gm,
+                        (match) => match.length === 3 ? '---' : '<bp/>'
+                    ).replace(/^\n$/gm, '<br/>')}
+                </Markdown>
             </div>
-            <div className=" preview-area-2 p-2 relative ">
-                <div
-                    style={{
-                        scale: width / 850
-                    }}
-                    className="border-2 page markdown-content"
-                >
-                    <ReactMarkdown options={options}>
-                        {mdValue.replace(
-                            /^([-]{3,})$/gm,
-                            (match) => match.length === 3 ? '---' : '<bp/>'
-                        ).replace(/^\n$/gm, '<br/>')}
-                    </ReactMarkdown>
-                </div>
-            </div>
-        </ div>
-    )
+        </div>
+    </ div>
+)
 }
 
 export default PreviewArea
